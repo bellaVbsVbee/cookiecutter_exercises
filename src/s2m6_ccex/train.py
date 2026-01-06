8c3e7ea68ba62801e12d18200765ccab2710cf26
bella <s214205@dtu.dk>
2026-01-06 11:07:04 +0100

    init cookiecutter project

---
+++ src/s2m6_ccex/train.py
@@ -0,0 +1,10 @@
+from s2m6_ccex.model import Model
+from s2m6_ccex.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
