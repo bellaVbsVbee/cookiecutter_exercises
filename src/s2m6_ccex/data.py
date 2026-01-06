770093645eaa250f8860980bf66da7f3453abca8
bella <s214205@dtu.dk>
2026-01-06 12:07:01 +0100

    Fixed paths

--- src/s2m6_ccex/data.py
+++ src/s2m6_ccex/data.py
@@ -13,15 +13,16 @@ def normalize(images: torch.Tensor) -> torch.Tensor:
 
 def preprocess(data_path: Path, output_folder: Path) -> None:
     print("Preprocessing data...")
-
-    train_image_files = glob.glob(os.path.join(data_path, "/train_images*.pt"))
-    train_label_files = glob.glob(os.path.join(data_path, "/train_target*.pt"))
+    data_path = str(data_path)
+    
+    train_image_files = glob.glob(f"{data_path}/corruptmnist_v1/train_images_*.pt")
+    train_label_files = glob.glob(f"{data_path}/corruptmnist_v1/train_target_*.pt")
 
     train_images = torch.cat([torch.load(f) for f in train_image_files], dim=0)
     train_labels = torch.cat([torch.load(f) for f in train_label_files], dim=0)
 
-    test_images = torch.load(data_path + "/test_images.pt")
-    test_labels = torch.load(data_path + "/test_target.pt")
+    test_images = torch.load(data_path + "/corruptmnist_v1/test_images.pt")
+    test_labels = torch.load(data_path + "/corruptmnist_v1/test_target.pt")
     
     train_images = train_images.unsqueeze(1).float()
     test_images = test_images.unsqueeze(1).float()
@@ -29,18 +30,18 @@ def preprocess(data_path: Path, output_folder: Path) -> None:
     train_images = normalize(train_images)
     test_images = normalize(test_images)
 
-    torch.save(train_images, f"{output_folder}/corrupt_mnist_v1/train_images.pt")
-    torch.save(train_labels, f"{output_folder}/corrupt_mnist_v1/train_target.pt")
-    torch.save(test_images, f"{output_folder}/corrupt_mnist_v1/test_images.pt")
-    torch.save(test_labels, f"{output_folder}/corrupt_mnist_v1/test_target.pt")
+    torch.save(train_images, f"{output_folder}/corruptmnist_v1/train_images.pt")
+    torch.save(train_labels, f"{output_folder}/corruptmnist_v1/train_target.pt")
+    torch.save(test_images, f"{output_folder}/corruptmnist_v1/test_images.pt")
+    torch.save(test_labels, f"{output_folder}/corruptmnist_v1/test_target.pt")
 
 def corrupt_mnist():
     """Return train and test datasets for corrupt MNIST."""
     
-    train_images = torch.load("~/data/processed/corrupt_mnist_v1/train_images.pt")
-    train_target = torch.load("~/data/processed/corrupt_mnist_v1/train_target.pt")
-    test_images = torch.load("~/data/processed/corrupt_mnist_v1/test_images.pt")
-    test_target = torch.load("~/data/processed/corrupt_mnist_v1/test_target.pt")
+    train_images = torch.load("~/data/processed/corruptmnist_v1/train_images.pt")
+    train_target = torch.load("~/data/processed/corruptmnist_v1/train_target.pt")
+    test_images = torch.load("~/data/processed/corruptmnist_v1/test_images.pt")
+    test_target = torch.load("~/data/processed/corruptmnist_v1/test_target.pt")
 
     train_set = torch.utils.data.TensorDataset(train_images, train_target)
     test_set = torch.utils.data.TensorDataset(test_images, test_target)
