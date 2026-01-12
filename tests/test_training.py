d8f42d0bf3f241275090902171891259304ad714
bella <s214205@dtu.dk>
2026-01-12 12:42:50 +0100

    Loading model in stead of training

--- tests/test_training.py
+++ tests/test_training.py
@@ -2,17 +2,27 @@ import torch
 import pytest
 from s2m6_ccex.train2 import train
 from s2m6_ccex.model2 import Model
-
+import os
 
 def test_my_training_loop():
-    torch.manual_seed(0)
 
-    trained_model = train()
-    trained_model.eval()
+    torch.manual_seed(0)
+    model = Model()
+
+    root = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "../..")
+    )
+    
+    models_dir = os.path.join(root, "models")
+    save_path = os.path.join(models_dir, "corrupt_mnist_checkpoint.pth")
+    state_dict = torch.load(save_path)
+    model.load_state_dict(state_dict)
+
+    model.eval()
 
     with torch.no_grad():
         dummy_input = torch.randn(5, 1, 28, 28)
-        new_output = trained_model(dummy_input)
+        new_output = model(dummy_input)
         preds = new_output.argmax(dim=1)
 
         assert new_output.shape == (5, 10)
